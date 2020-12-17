596d20818affce768c706664574ba511b96d9a95
Pascal Strack <pstrack@hotmail.de>
2020-12-17 08:45:24 +0100

    aufgabe2

---
+++ Aufgabe2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	int w{ 1 };
+	int v{ 1 };
+	int a{ 1 };
+	int b{ 1 };
+	int c{ 1 };
+
+	for (int counter{ 1 }; counter < 500; counter++)
+	{
+		w++;
+		v* v - w * w = a
+			cout >> a >> endl;
+	}
+
+
+}
